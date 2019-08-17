587e13ae99d292ff5f59a7facc1a642ec0c16587
Carlo Ortega <cfor0@outlook.com>
2019-08-16 21:22:12 -0400

    questions change after time is up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,39 +2,86 @@ $(document).ready(function () {
     $(".questions").hide();
     // game questions array
     var gameQuestions = [
-        { question: "What is my last name?",
-        choices: ["Neely", "Ortega", "Hair", "Kevin"], 
-        correctAnswer: "Ortega"
+        {
+            question: "What is my last name?",
+            choices: ["Neely", "Ortega", "Hair", "Kevin"],
+            correctAnswer: "Ortega"
         },
-        { question: "What is my first name?",
-        choices: ["Josh", "Carlo", "Juan", "Kevin"], 
-        correctAnswer: "Carlo"
+        {
+            question: "What is my first name?",
+            choices: ["Josh", "Carlo", "Juan", "Kevin"],
+            correctAnswer: "Carlo"
         },
-        { question: "What how old am I?",
-        choices: ["24", "19", "23", "22"], 
-        correctAnswer: "22"
+        {
+            question: "What how old am I?",
+            choices: ["24", "19", "23", "22"],
+            correctAnswer: "22"
         },
-        { question: "What is my favorite color?",
-        choices: ["green", "yellow", "blue", "Kevin"], 
-        correctAnswer: "green"
+        {
+            question: "What is my favorite color?",
+            choices: ["green", "yellow", "blue", "Kevin"],
+            correctAnswer: "green"
         },
-        { question: "What was my major in?",
-        choices: ["computer science", "finance", "marketing", "music"], 
-        correctAnswer: "music"
+        {
+            question: "What was my major in?",
+            choices: ["computer science", "finance", "marketing", "music"],
+            correctAnswer: "music"
         },
     ];
     console.log(gameQuestions)
+
     // load game Questions
     var currentQuestion = 0;
-    
+    var counter = 30;
+    //creating variable for timer in the global scope
+    var timer;
+
+    function nextQuestion() {
+        var isQuestionOver = (gameQuestions.length - 1) === currentQuestion;
+        if (isQuestionOver) {
+
+        } else {
+            currentQuestion++;
+            loadQuestions();
+        }
+    }
+
+    // create count down timer with counter variable
+    function timeup() {
+        clearInterval(timer);
+        nextQuestion();
+    }
+    function countdown() {
+        counter--;
+        $('#timer').html(`Time Remaining: ` + counter);
+        if (counter === 0) {
+            timeup();
+        }
+    }
+
     function loadQuestions() {
+        counter = 30;
+        timer = setInterval(countdown, 1000);
+
         var question = gameQuestions[currentQuestion].question;
         var choices = gameQuestions[currentQuestion].choices;
-        $("#timer").html("Time Remaining: " + )
-        $("#game").html("<h4>" + question + "</h4>");
+        $("#timer").html(`Time Remaining: ` + counter);
+        $("#game").html(`
+        <h4>${question}</h4>
+        ${loadChoices(choices)}
+        `);
+    }
+
+
+    //load game choices
+    function loadChoices(choices) {
+        var result = '';
+        for (var i = 0; i < choices.length; i++) {
+            result += `<p class ="choice" data-answer = "${choices[i]}">${choices[i]} </p>`;
+        }
+        return result;
     }
     loadQuestions();
-    //load game choices
 
     // make start button hide and bring up questions onclick
     $("#start").click(function startTimer() {
@@ -42,11 +89,7 @@ $(document).ready(function () {
         $(".questions").show();
 
 
-        setInterval(thirtySec, 30000);
-        function thirtySec() {
-           
-            console.log("works")
-        }
+
     });
 
     // create variable for setTimeout
